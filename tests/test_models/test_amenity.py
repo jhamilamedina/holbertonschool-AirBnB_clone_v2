45b321d5f5d358801cf7a8c7570555bd808180a9
Georgia2292 <georgelina2292@gmail.com>
2024-01-21 07:18:21 -0800

    testing

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -12,9 +12,10 @@ import inspect
 import unittest
 storage_t = getenv("HBNB_TYPE_STORAGE")
 
-class test_Amenity(test_basemodel):
-    """ """
 
+class test_Amenity(test_basemodel):
+
+    """ """
     def __init__(self, *args, **kwargs):
         """ """
         super().__init__(*args, **kwargs)
